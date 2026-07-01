9e340a1f4aed2c07f2dc1ae815615e6922e50c9a
Armand Thuillart <136462994+armandthuillart@users.noreply.github.com>
2026-07-02 00:17:53 +0200

    chore: some more cleaning

--- astro.config.ts
+++ astro.config.ts
@@ -6,21 +6,49 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
   fonts: [
     {
-      cssVariable: '--font-fira-code',
-      name: 'Fira Code',
-      provider: fontProviders.fontsource(),
-      weights: ['100 900'],
+      cssVariable: '--font-newyork',
+      name: 'New York',
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/NewYork.woff2'],
+            style: 'normal',
+            weight: '100 900',
+          },
+        ],
+      },
     },
     {
-      cssVariable: '--font-inter',
-      name: 'Inter',
-      provider: fontProviders.fontsource(),
-      weights: ['100 900'],
+      cssVariable: '--font-sf-mono',
+      name: 'SF Mono',
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/SF-Mono.woff2'],
+            style: 'normal',
+            weight: '400',
+          },
+        ],
+      },
+    },
+    {
+      cssVariable: '--font-sf-pro',
+      name: 'SF Pro',
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ['./src/assets/fonts/SF-Pro.woff2'],
+            style: 'normal',
+            weight: '100 900',
+          },
+        ],
+      },
     },
   ],
   integrations: [mdx(), sitemap()],
   site: 'https://armandthuillart.com',
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: { plugins: [tailwindcss()] },
 });
