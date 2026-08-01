ae6caa73ff7e4a73f5897a85781dd59141b93a88
Armand Thuillart <armandthuillart@gmail.com>
2026-08-01 18:32:04 +0200

    Add page-specific SEO descriptions

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
   schema: () =>
     z.object({
       date: z.coerce.date(),
+      description: z.string(),
       draft: z.boolean().default(false),
       index: z.boolean().default(false),
       title: z.string(),
